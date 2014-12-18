d59f61fe5e3b35fd24b18dbfa785a0c9a9275fa6
prathibha-mv <prathihamv92@gmail.com>
2014-12-18 10:44:05 +0530

    App

---
+++ DrugAnalyser/src/com/datumdroid/android/ocr/simple/DrugComposition.java
@@ -0,0 +1,9 @@
+package com.datumdroid.android.ocr.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrugComposition {
+	  public String name;
+	    public List<DrugCompound> compounds = new ArrayList<DrugCompound>();
+}
